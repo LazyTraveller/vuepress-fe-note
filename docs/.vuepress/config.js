2900ec8fa41feb80ab6935cf3e6e3225eded26ab
heweizhi <heweizhi@guanmai.cn>
2021-06-18 15:37:53 +0800

    note

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const path = require('path')
+const process = require('process')
+
+const ignoreList = ['.vuepress']
+const workPath = path.join(process.cwd() + '/docs')
+
+function buildChildren(path, parentName = '') {
+  const files = fs.readdirSync(path)
+  return files.map((file) => {
+    if (ignoreList.includes(file)) return
+    const current = { title: file }
+    const subPath = `${path}/${file}`
+    if (fs.statSync(subPath).isDirectory()) {
+      current.children = buildChildren(subPath, `${parentName}/${file}`)
+    } else {
+      if (file === 'README.md') {
+        current.path = `${parentName}/`
+      } else {
+        const suffixName = file.slice(-3)
+        if (suffixName !== '.md') return
+        current.path = `${parentName}/${file.slice(0, -3)}`
+      }
+    }
+    return current
+  }).filter((item) => item)
+}
+
+
+const sidebar = buildChildren(workPath)
+module.exports = {
+  title: '前端随笔',
+  description: '前端系统知识笔记',
+  themeConfig: {
+    nav: [{ text: 'GitHub', link: "https://github.com/LazyTraveller/vuepress-fe-note" }],
+    sidebar,
+  },
+  dest: path.resolve(__dirname, '../', '../', 'dist'),
+  base: '/vuepress-fe-note/',
+  evergreen: true,
+}
