6f73007363a242ce159f5efc37dfaebf663c125f
heweizhi <heweizhi@guanmai.cn>
2021-09-27 17:55:57 +0800

    feat: promise

--- docs/代码/code_factory.js
+++ docs/代码/code_factory.js
@@ -1,7 +1,30 @@
-function d () {
-  return Promise((rvo, rej) => {
+// promise 封装
 
-  }).then(() => {
+function getJSON(url) {
+  let promise = new Promise(function(resolve, reject) {
+    let xhr = new XMLHttpRequest()
 
+    xhr.open('GET', url, true)
+
+    xhr.onreadystatechange = function() {
+      if (this.readyState !== 4) return 
+      if (this.readyState === 200) {
+        resolve(this.response)
+      } else {
+        reject(new Error(this.statusText))
+      }
+    }
+
+    xhr.onerror =  function() {
+      reject(new Error(this.statusText))
+    }
+
+    xhr.responseType = 'json'
+
+    xhr.setRequestHeader('Accept', 'application/json')
+
+    xhr.send(null)
+
+    return promise
   })
 }
